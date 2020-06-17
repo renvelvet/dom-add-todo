fecf01790e9662fdd00c75b6a1b01bff1fa9f865
Resha Puspita <reshapuspita734@gmail.com>
2020-06-17 21:10:04 +0700

    update code sesuai pembahasan

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 // -------------------- Variable -------------------------
 let divList = document.querySelector(".display-todo");
 let listTodo = [];
-
-const addButton = document.getElementById("add-button");
+let addButton = document.getElementById("add-button");
 
 // -------------------- Functions -------------------------
+displayTodos = () => {
+  divList.innerHTML = null;
+
+  listTodo.forEach((item, index) => {
+    // div todo
+    let itemTodo = document.createTextNode(item);
+    let textTodo = document.createElement("p");
+    textTodo.appendChild(itemTodo);
+    let divTodo = createDiv("class", "todo").appendChild(textTodo);
+
+    // div button
+    let divButton = createDiv("class", "button");
+    let editbutton = createButton("edit");
+    let deleteButton = createButton("delete");
+    divButton.appendChild(editbutton);
+    divButton.appendChild(deleteButton);
+
+    // divList
+    let list = createDiv("class", "list");
+    list.appendChild(divTodo);
+    list.appendChild(divButton);
+    divList.appendChild(list);
+
+    // event edit delete
+    editbutton.addEventListener("click", () => {
+      editItem(index);
+    });
+    deleteButton.addEventListener("click", () => {
+      deleteItem(index);
+    });
+  });
+};
+
 createButton = (element) => {
   let button = document.createElement("button");
   button.setAttribute("type", "button");
@@ -22,33 +54,26 @@ createDiv = (attName, valueAtt) => {
   return div;
 };
 
-// --------------------EVEN LISTENER -------------------------
+editItem = (index) => {
+  newItem = prompt("Mau edit jadi apa?");
+  listTodo.splice(index, 1, newItem);
+
+  displayTodos();
+};
+
+deleteItem = (index) => {
+  listTodo.splice(index, 1);
+
+  displayTodos();
+};
+// --------------------EVENT LISTENER -------------------------
 // add button clicked
-addButton.addEventListener("click", function () {
-  divList.innerHTML = null;
+addButton.addEventListener("click", () => {
   const inputTodo = document.getElementById("inputTodo").value;
   listTodo.push(inputTodo);
-
-  listTodo.forEach((item) => {
-    // item list
-    let itemTodo = document.createTextNode(item);
-    let textTodo = document.createElement("p");
-    textTodo.appendChild(itemTodo);
-
-    // div todo
-    let divTodo = createDiv("class", "todo").appendChild(textTodo);
-
-    // div button
-    let divButton = createDiv("class", "button");
-    divButton.appendChild(createButton("edit"));
-    divButton.appendChild(createButton("delete"));
-
-    // list
-    let list = createDiv("class", "list");
-    list.appendChild(divTodo);
-    list.appendChild(divButton);
-
-    // masukin ke divList
-    divList.appendChild(list);
-  });
+  displayTodos();
 });
+
+// edit button clicked
+
+// delete button clicked
